0aa1102e4fee03eff5150b0e2c6a2d6710e7ad65
isaiah francois <francoisisaiaht@yahoo.com>
2020-11-01 15:18:04 -0500

    Added missing files

---
+++ components/Building.js
@@ -0,0 +1,15 @@
+import React, { memo } from 'react';
+import { Image, StyleSheet } from 'react-native';
+
+const Building = () => (
+  <Image source={require('../assets/building.png')} style={styles.image} />
+);
+
+const styles = StyleSheet.create({
+  image: {
+    width: 32,
+    height: 32,
+  },
+});
+
+export default memo(Building);
