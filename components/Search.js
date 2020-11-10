e19b6e1d8f2a4ee95b90accafe5929fc7e38e86a
Samuel Obe <obes@rpi.edu>
2020-11-10 11:51:55 -0500

    Updated NPM packages

--- components/Search.js
+++ components/Search.js
@@ -1,21 +1,30 @@
-import React from "react";
-import { StyleSheet, View, Text, Button, Card } from "react-native";
+import * as React from "react";
+import { Searchbar } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
 import { theme } from "../core/theme";
-import Icon from "react-native-vector-icons/FontAwesome";
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: theme.colors.background,
   },
-
+  searchbar:{
+    margin: "2%"
+  },
 });
 
 const Search = () => {
+  const [searchQuery, setSearchQuery] = React.useState("");
 
-
+  const onChangeSearch = (query) => setSearchQuery(query);
   return (
     <View style={styles.container}>
-
+      <Searchbar
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+        style={styles.searchbar}
+      />
     </View>
   );
 };
