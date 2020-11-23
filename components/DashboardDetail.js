cce242b7fc4050b170dddf46be4d6d42b3071e11
Samuel Obe <obes@rpi.edu>
2020-11-23 10:10:40 -0500

    Removed map from detail page

--- components/DashboardDetail.js
+++ components/DashboardDetail.js
@@ -11,8 +11,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  map: {
+  upperView: {
     flex: 2,
+    justifyContent: "center",
+    alignItems: "center",
   },
   bottomView: {
     flex: 1,
@@ -36,59 +38,11 @@ const DashboardDetail = ({ route, navigation }) => {
     user,
   } = route.params;
 
-  const origin = { latitude: 32.346449, longitude: -84.973891 };
-  const destination = { latitude: coordinates[0], longitude: coordinates[1] };
-  const GOOGLE_MAPS_APIKEY = "AIzaSyAY6tGYbs1FdLLDopXBWRQAMqEhhvyFtRU";
-
-  let userLocation = { latitude: null, longitude: null };
-
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestPermissionsAsync();
-      if (status !== "granted") {
-        setErrorMsg("Permission to access location was denied");
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
-
-  if (!errorMsg && location) {
-    userLocation.latitude = location.coords.latitude;
-    userLocation.longitude = location.coords.longitude;
-  }
-
   return (
     <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        showsUserLocation={true}
-        moveOnMarkerPress={false}
-        style={styles.map}
-        initialRegion={{
-          latitude: 32.344,
-          longitude: -84.976813,
-          latitudeDelta: 0.011,
-          longitudeDelta: 0.003,
-        }}
-      >
-        <MapViewDirections
-          origin={origin}
-          destination={destination}
-          apikey={GOOGLE_MAPS_APIKEY}
-          strokeColor="hotpink"
-          strokeWidth={3}
-        />
-
-        <Marker coordinate={origin} title={"My Location"} />
-        <Marker coordinate={destination}>
-          <Building />
-        </Marker>
-      </MapView>
+      <View style={styles.upperView}>
+        <Text>Insert Image Here</Text>
+      </View>
       <View style={styles.bottomView}>
         <Title style={styles.text}>Room:</Title>
         <Text style={styles.text}>{room}</Text>
