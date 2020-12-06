9ffda953c3ae4c5cda3b71b56f6f3f34e3c85956
Samuel Obe <obes@rpi.edu>
2020-12-05 23:02:44 -0500

    Created LoginContainer and MainContainer files

---
+++ components/LoginContainer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { StatusBar } from "react-native";
+
+// Theme
+import { theme } from "./core/theme";
+
+// Navigation
+import { NavigationContainer } from "@react-navigation/native";
+
+const LoginContainer = ({ Tab, user }) => {
+  return (
+    <NavigationContainer>
+      <StatusBar
+        backgroundColor={theme.colors.primary}
+        barStyle="light-content"
+      />
+      <Tab.Navigator initialRouteName="Login">
+        <Tab.Screen
+          name="Login"
+          component={LoginPage}
+          options={{ tabBarVisible: false }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default LoginContainer;
---
+++ components/MainContainer.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { StatusBar } from "react-native";
+import MapPage from "./pages/MapPage";
+import DashboardPage from "./pages/DashboardPage";
+import SettingsPage from "./pages/SettingsPage";
+
+// Theme
+import { theme } from "./core/theme";
+
+//Icons
+//cheatsheet: https://ionicons.com/v4/cheatsheet.html
+import Icon from "react-native-vector-icons/Ionicons";
+
+// Navigation
+import { NavigationContainer } from "@react-navigation/native";
+
+const MainContainer = ({ Tab, user }) => {
+  return (
+    <NavigationContainer>
+      <StatusBar
+        backgroundColor={theme.colors.primary}
+        barStyle="light-content"
+      />
+      <Tab.Navigator
+        initialRouteName="Map"
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            switch (route.name) {
+              case "Map":
+                iconName = "ios-map";
+                break;
+              case "Dashboard":
+                iconName = "md-list";
+                break;
+              case "Settings":
+                iconName = "md-settings";
+                break;
+
+              default:
+                break;
+            }
+
+            // You can return any component that you like here!
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: theme.colors.secondary,
+          inactiveTintColor: theme.colors.accent,
+          style: {
+            backgroundColor: theme.colors.primary,
+            borderTopWidth: 0,
+          },
+        }}
+      >
+        <Tab.Screen name="Map" component={MapPage} />
+        <Tab.Screen name="Dashboard" component={DashboardPage} />
+        <Tab.Screen name="Settings" component={SettingsPage} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default MainContainer;
