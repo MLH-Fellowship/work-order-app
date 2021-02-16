89a6d1bcd1779b760e664fd3b782a7188913a183
NoahCardoza <noahcardoza@gmail.com>
2021-02-16 15:18:21 -0800

    default form input to 100% width

--- components/FormInput.js
+++ components/FormInput.js
@@ -5,7 +5,7 @@ const styles = StyleSheet.create({
   input: {
     backgroundColor: "white",
     overflow: "hidden",
-    width: "80%",
+    width: "100%",
     height: 32,
     padding: 10,
     marginBottom: 10,
