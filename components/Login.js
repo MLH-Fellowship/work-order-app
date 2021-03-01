fbb11e9d78318a18cd3028451967d4219f131ed0
NoahCardoza <noahcardoza@gmail.com>
2021-02-28 16:32:42 -0800

    move Login to native-base and disable validation since its a login form

--- components/Login.js
+++ components/Login.js
@@ -1,27 +1,27 @@
 import React, { memo, useState } from "react";
 import { StyleSheet, KeyboardAvoidingView, Platform } from "react-native";
-import { Toast, Text, Container, Button, Spinner } from "native-base";
-import Logo from "../components/Logo";
-import TextInput from "../components/TextInput";
-import { emailValidator, passwordValidator } from "../core/utils";
+import { Toast, Text, Container, Button, Spinner, Input, Item, Label } from "native-base";
+import Logo from "../components/Logo";;
+// import { emailValidator, passwordValidator } from "../core/utils";
 import { loginUser } from "../api/auth-api";
 
 const LoginScreen = ({ navigation }) => {
-  const [email, setEmail] = useState({ value: "", error: "" });
-  const [password, setPassword] = useState({ value: "", error: "" });
+  const [email, setEmail] = useState({ value: "", error: undefined });
+  const [password, setPassword] = useState({ value: "", error: undefined });
   const [loading, setLoading] = useState(false);
 
   const onLoginPressed = async () => {
     if (loading) return;
 
-    const emailError = emailValidator(email.value);
-    const passwordError = passwordValidator(password.value);
+    // TODO: move this to signup not login
+    // const emailError = emailValidator(email.value);
+    // const passwordError = passwordValidator(password.value);
 
-    if (emailError || passwordError) {
-      setEmail({ ...email, error: emailError });
-      setPassword({ ...password, error: passwordError });
-      return;
-    }
+    // if (emailError || passwordError) {
+    //   setEmail({ ...email, error: emailError });
+    //   setPassword({ ...password, error: passwordError });
+    //   return;
+    // }
 
     setLoading(true);
 
@@ -57,29 +57,30 @@ const LoginScreen = ({ navigation }) => {
       >
         <Logo />
 
-        <TextInput
-          label="Email"
-          returnKeyType="next"
-          value={email.value}
-          onChangeText={(text) => setEmail({ value: text, error: "" })}
-          error={!!email.error}
-          errorText={email.error}
-          autoCapitalize="none"
-          autoCompleteType="email"
-          textContentType="emailAddress"
-          keyboardType="email-address"
-        />
+        <Item floatingLabel error={email.error !== undefined}>
+          <Label>Email</Label>
+          <Input
+            keyboardType="email-address"
+            textContentType="emailAddress"
+            autoCompleteType="email"
+            autoCapitalize="none"
+            value={email.value}
+            onChangeText={value =>  setEmail({ value })}
+          />
+        </Item>
 
-        <TextInput
-          label="Password"
-          returnKeyType="done"
-          value={password.value}
-          onChangeText={(text) => setPassword({ value: text, error: "" })}
-          error={!!password.error}
-          errorText={password.error}
-          secureTextEntry
-          autoCapitalize="none"
-        />
+        <Item floatingLabel error={password.error !== undefined} style={{marginVertical: 20 }}>
+          <Label>Password</Label>
+          <Input
+            textContentType="password"
+            autoCompleteType="password"
+            autoCapitalize="none"
+            passwordRules="minlength: 7; maxlength: 20; required: lower; required: upper; required: digit;"
+            secureTextEntry
+            value={password.value}
+            onChangeText={value =>  setPassword({ value })}
+          />
+        </Item>
         <Button disabled={loading} primary={!loading} block onPress={onLoginPressed}>
           {loading && <Spinner /> || <Text>Login</Text>}
         </Button>
