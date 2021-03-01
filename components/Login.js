5385dfb1c0e3d761594092105cdc457bc312eef8
Andrei <andrei.nexus@gmail.com>
2021-03-01 00:22:05 -0800

    Rebase

--- components/Login.js
+++ components/Login.js
@@ -43,6 +43,8 @@ const LoginScreen = ({ navigation }) => {
 
   const styles = StyleSheet.create({
     container: {
+      //flex:1, 
+      flexDirection:'column', 
       justifyContent: "center",
       alignItems: "center",
       margin: "5%",
