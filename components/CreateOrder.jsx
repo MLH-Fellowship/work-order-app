040b21958f7ac3078c3fe769baf40ee0e3b6b2aa
NoahCardoza <noahcardoza@gmail.com>
2021-03-03 09:13:10 -0800

    remove duplicate from more action logic to actions js file

--- components/CreateOrder.jsx
+++ components/CreateOrder.jsx
@@ -2,13 +2,13 @@ import { Formik } from 'formik';
 
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addOrders, deactivateModal } from '../actions/index';
 import FormButton from './FormButton';
 import FormInput from './FormInput';
 
 import theme from '../native-base-theme/variables/commonColor';
-import ImageSelectButtonGroup, { uploadImage } from './ImageSelectionButtonGroup';
+import ImageSelectButtonGroup from './ImageSelectionButtonGroup';
 
 const styles = StyleSheet.create({
   container: {
@@ -35,91 +35,24 @@ const styles = StyleSheet.create({
 
 const CreateOrder = ({ buildingNumber, buildingCoordinates }) => {
   const dispatch = useDispatch();
-
-  if (!buildingNumber) {
-    return (
-      <Formik
-        initialValues={{
-          user: 'testuser',
-          problem: '',
-          description: '',
-          image: null,
-          coordinates: buildingCoordinates,
-          complete: false,
-        }}
-        // TODO: Add validation schema
-        onSubmit={(values) => {
-          if (values.image != null) {
-            const { image } = values;
-            uploadImage(image, values);
-            // TODO: Minimize problem name
-          } else {
-            addOrders(values)(dispatch);
-          }
-          dispatch(deactivateModal());
-        }}
-      >
-        {({
-          handleChange, handleBlur, handleSubmit, values,
-        }) => {
-          const { ImagePreview, ButtonGroup } = ImageSelectButtonGroup({
-            form: values,
-            onChange: handleChange,
-            formProp: 'image',
-          });
-          return (
-            <View style={styles.container}>
-              <FormInput
-                placeholder="Problem"
-                onChangeText={handleChange('problem')}
-                onBlur={handleBlur('problem')}
-                value={values.problem}
-              />
-              <FormInput
-                placeholder="Description"
-                multiline
-                style={styles.descriptionTextbox}
-                numberOfLines={2}
-                onChangeText={handleChange('description')}
-                onBlur={handleBlur('description')}
-                value={values.description}
-              />
-
-              <ImagePreview />
-
-              <View style={styles.buttonGroupParanet}>
-                <ButtonGroup />
-                <FormButton onSubmit={handleSubmit} style={styles.submitButton} text="Submit" />
-              </View>
-
-            </View>
-          );
-        }}
-      </Formik>
-    );
-  }
-
+  const userReducer = useSelector((state) => state.userReducer);
   return (
     <Formik
       initialValues={{
-        user: 'testuser',
-        building: buildingNumber,
         room: '',
         problem: '',
         description: '',
         image: null,
-        coordinates: buildingCoordinates,
-        complete: false,
       }}
-        // TODO: Add validation schema
       onSubmit={(values) => {
-        if (values.image != null) {
-          const { image } = values;
-          uploadImage(image, values);
-          // TODO: Minimize problem name
-        } else {
-          addOrders(values)(dispatch);
-        }
+        // TODO: Add validation schema?
+        // TODO: maybe add some loading?
+        addOrders({
+          ...values,
+          user: userReducer.username,
+          building: buildingNumber,
+          coordinates: buildingCoordinates,
+        })(dispatch);
         dispatch(deactivateModal());
       }}
     >
