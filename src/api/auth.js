4f6dfdb79618f7f92402e7c3cb72c014c199166e
NoahCardoza <noahcardoza@gmail.com>
2021-03-11 22:19:45 -0800

    clean up switch in auth

--- src/api/auth.js
+++ src/api/auth.js
@@ -5,42 +5,23 @@ export const logoutUser = () => {
   firebase.auth().signOut();
 };
 
-export const registerUser = async ( email, password, data ) => {
-  let user = null;
-  await firebase.auth().createUserWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    console.log('attempting to add data to db');
-    createUserInfo(userCredential.user.uid, data);
-  })
-  .catch((error) => {
-    switch (error.code) {
-      case "auth/email-already-in-use":
-        return {
-          message: '',
-          error: 'Unable to create account. E-mail already in use.'
-        };
-      case "auth/invalid-email":
-        return {
-          message: '',
-          error: 'Unable to create account. Invalid e-mail address format.'
-        };
-      case "auth/weak-password":
-        return {
-          message: '',
-          error: 'Password is too weak.'
-        };
-      case "auth/too-many-requests":
-        return {
-          message: '',
-          error: 'Unable to create account. Try again in a minute.'
-        };
-      default:
-        return {
-          message: '',
-          error: 'Unable to create account. Please check your internet connection.'
-        };
+export const registerUser = async (email, password, data) => {
+  try {
+    const { user } = await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+    createUserInfo(user.uid, data);
+  } catch (e) {
+    return {
+      message: '',
+      error: {
+        'auth/email-already-in-use': 'Unable to create account. E-mail already in use.',
+        'auth/invalid-email': 'Unable to create account. Invalid e-mail address format.',
+        'auth/weak-password': 'Password is too weak.',
+        'auth/too-many-requests': 'Unable to create account. Try again in a minute.',
+      }[e.code] || 'Unable to create account. Please check your internet connection.'
     }
-  });
+  }
 
   return {
     message: 'Verification email sent',
@@ -50,12 +31,14 @@ export const registerUser = async ( email, password, data ) => {
 };
 
 export const loginUser = async ({ email, password }) => {
-  console.log('LOGIN OBJECT', email, password);
   try {
-    await firebase.auth().signInWithEmailAndPassword(email, password);
-    return {error: ''};
+    await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password);
+    return {
+      error: ''
+    };
   } catch (error) {
-    console.log(error);
     switch (error.code) {
       case 'auth/invalid-email':
         return {
