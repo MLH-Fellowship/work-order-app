cc2d28307269328b0e20ef72f35c53bf7eb95418
NoahCardoza <noahcardoza@gmail.com>
2021-03-11 22:10:13 -0800

    move action logic into store

--- src/store/order.js
+++ src/store/order.js
@@ -1,3 +1,7 @@
+import firebase from "@/api/firebase";
+import { uploadImage } from "@/api/storage";
+
+
 const INITIAL_STATE = {
   orders: {},
   userOrders: {},
@@ -9,12 +13,57 @@ const GET_USER_ORDERS = 'GET_USER_ORDERS';
 const ADD_ORDERS = 'ADD_ORDERS';
 const UPDATE_ORDERS = 'UPDATE_ORDERS';
 
-export const types = {
-  GET_ORDERS,
-  GET_USER_ORDERS,
-  ADD_ORDERS,
-  UPDATE_ORDERS,
-}
+// START: ACTIONS
+export const getOrders = () => (dispatch) => {
+  firebase
+    .database()
+    .ref('/orders')
+    .on('value', (snap) => {
+    const data = snap.val() ? snap.val() : {};
+    dispatch({ type: GET_ORDERS, payload: data });
+  });
+};
+
+export const getUserOrders = (user) => (dispatch) => {
+  firebase
+    .database()
+    .ref('/orders')
+    .on('value', (snap) => {
+    const data = snap.val() ? snap.val() : {};
+    console.log(data);
+    // let orders = Object.values(data);
+
+    dispatch({
+      type: GET_USER_ORDERS,
+      payload: data,
+      // payload: orders.filter(
+      //   (order) => order.user == user && order.complete == false
+      // ),
+    });
+  });
+};
+
+export const updateOrders = (orderName, data) => {
+  firebase
+    .database()
+    .ref('/orders')
+    .child(orderName)
+    .set(data)
+};
+
+export const addOrders = (order) => async (dispatch) => {
+  dispatch({ type: ADD_ORDERS });
+  const image = order.image !== null ? await uploadImage(order.image, uuidv4()) : null;
+  firebase
+    .database()
+    .ref('/orders')
+    .push({
+    ...order,
+    image,
+    issue_opened_on: firebase.database.ServerValue.TIMESTAMP,
+  });
+};
+// END: ACTIONS
 
 const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
--- src/store/user.js
+++ src/store/user.js
@@ -1,7 +1,5 @@
 import firebase from '@/api/firebase';
 
-const db = firebase.database();
-
 const INITIAL_STATE = {
   username: '',
   role: '',
@@ -11,13 +9,15 @@ const SET_CURRENT_USER = 'SET_CURRENT_USER';
 
 // START: ACTIONS
 export const setCurrentUser = (username) => (dispatch) => {
-  db.ref('/users')
+  firebase
+    .database()
+    .ref('/users')
     .child(username)
     .on('value', (snap) => {
       // TODO: remove { role: 'admin' }
       const data = snap.val() || { role: 'admin' };
       dispatch({
-        type: actionTypes.SET_CURRENT_USER,
+        type: SET_CURRENT_USER,
         username,
         role: data.role,
       });
