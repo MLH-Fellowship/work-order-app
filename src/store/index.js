960cc4f2c2385243c6d4a7e7f59e1116532080da
NoahCardoza <noahcardoza@gmail.com>
2021-03-17 11:21:49 -0700

    remove modal logic from store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
-import modalReducer from "./modal";
 import userReducer from "./user";
 import orderReducer from "./order";
 import dashboardReducer from "./dashboard";
 
 const rootReducer = combineReducers({
-  modalReducer,
   orderReducer,
   dashboardReducer,
   userReducer,
